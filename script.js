ff5b8c5a00b82adebd0216944c8e2e8cb447746a
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-20 14:57:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	let s2=""
+	for(let i=0; i<s.length; i++){
+		if(s[i]==" "){
+			return s2
+		}else{
+			s2+=s[i]
+		}
+	}
+	return s
+	
 }
 
 // Do not change the code below
