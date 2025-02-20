2b9521329638d358d6c2ca676a795cb83e754b42
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-20 15:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 function firstWord(s) {
   // your code here
+	let s1=s2.trim()
 	let s2=""
 	for(let i=0; i<s.length; i++){
-		if(s[i]==" "){
+		if(s1[i]==" "){
 			return s2
 		}else{
 			s2+=s[i]
 		}
 	}
-	return s
+	return s1
 	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
